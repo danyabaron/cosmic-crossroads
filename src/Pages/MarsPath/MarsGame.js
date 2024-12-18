9529495ad77173c4c52d72665e667a8fa781c54c
Danya Baron <danyabaron0@gmail.com>
2024-12-18 17:42:25 -0500

    added small changes to the new mars path branch

--- src/Pages/MarsPath/MarsGame.js
+++ src/Pages/MarsPath/MarsGame.js
@@ -4,8 +4,8 @@ import React from 'react';
 
 function MarsGame() {
     return (
-        <div className='bg-mars-bg-reg bg-cover bg-center h-screen'>
-            <h2 className="text-xl">
+        <div className='bg-mars-bg-reg bg-cover bg-center flex flex-col items-center h-screen'>
+            <h2 className="text-xl text-mars-red">
                 mars path
             </h2>
         </div>
