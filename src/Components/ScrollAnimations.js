4b5ee705d0b3e2c7359a19f2a666af236fc7d455
Danya Baron <danyabaron0@gmail.com>
2025-02-03 15:29:04 -0500

    got slider to work as well as other animations

--- src/Components/ScrollAnimations.js
+++ src/Components/ScrollAnimations.js
@@ -30,6 +30,10 @@ function ScrollAnimations()
 
     let config = useRef({strengh: 1});
 
+    const marsRef = useRef();
+    const projectileRef = useRef();
+    const asteroidRefs = useRef([]);
+
     useGSAP(() => {
         
         // line animation
@@ -39,7 +43,8 @@ function ScrollAnimations()
               scrub: true,
               pin: true,
               start: "top top",
-              end: "+=100%"
+              end: "+=100%",
+              markers: true,
             }
           });
           
@@ -54,8 +59,24 @@ function ScrollAnimations()
             0 // Start at the same time as the line
           );
 
+          //fade in color on line animation
+          tl.to( ".overlay", {
+            backgroundColor: "#F6EEC4",
+            opacity: 0.5,
+            duration: 3,
+            ease: "power1.inOut",
+            scrollTrigger: {
+                trigger: ".panel",
+                scrub: true,
+                start: "top center",
+                end: "+=100%",
+                
+            }
+          }, 0.5);
+          
+
           // intensify animation
-          gsap.to(".intensify", {
+          gsap.to(".mars-container", {
             repeat: -1,
             yoyo: true,
             x: 1,
@@ -80,9 +101,75 @@ function ScrollAnimations()
               console.log("Strength:", config.current.strength); // Debugging log to track strength value
             },
           });
+
+
+          gsap.to(projectileRef.current, {
+            x: '100vw',
+            duration: 2,
+            ease: 'power1.inOut',
+            scrollTrigger: {
+              trigger: '.planet-animation2',
+              start: 'top bottom',
+              end: 'bottom top',
+            //   scrub: true,
+              markers: true,
+              onEnter: () => console.log("Entered ScrollTrigger for projectile animation"), // Debugging log
+            onLeave: () => console.log("Left ScrollTrigger for projectile animation"), // Debugging log
+            },
+          });
+        // Animate asteroids
+            asteroidRefs.current.forEach((asteroid, index) => {
+                gsap.to(asteroid, {
+                y: '100vh', // Move down relative to starting position
+                x: `${Math.random() * 50 - 25}vw`, // Random horizontal movement
+                rotation: '+=360', // Rotate asteroids
+                duration: 1 + index, // Move faster by reducing duration
+                ease: 'power1.inOut',
+                scrollTrigger: {
+                    trigger: '.planet-animation2',
+                    // scrub: true,
+                    toggleActions: "restart pause restart pause",
+                    start: 'top bottom',
+                    end: 'bottom top',
+                    markers: true,
+                },
+                onComplete: () => {
+                    // Remove asteroid from view after it goes off-screen
+                    gsap.set(asteroid, { opacity: 0 });
+                },
+                });
+            });
+
+            // Function to shoot the projectile towards the asteroids
+            const shootProjectile = (targetAsteroid) => {
+                const asteroidRect = targetAsteroid.getBoundingClientRect();
+
+                gsap.to(projectileRef.current, {
+                x: asteroidRect.left + window.scrollX, // Adjust x position to target asteroid's x position
+                y: asteroidRect.top + window.scrollY,  // Adjust y position to target asteroid's y position
+                duration: 1, // Speed of projectile (adjsust as needed)
+                ease: 'power1.inOut',
+                onComplete: () => {
+                    // Optional: Impact effect or reset projectile
+                    gsap.set(projectileRef.current, { opacity: 0 });
+                },
+                });
+            };
+
+            // Add click event listeners to asteroids to trigger shooting
+            asteroidRefs.current.forEach((asteroid, index) => {
+                asteroid.addEventListener('click', () => {
+                shootProjectile(asteroid); // Shoot at clicked asteroid
+                });
+            });
+
+
+
         }, []);
 
 
+        console.log(projectileRef.current); // Should log the projectile DOM element
+    console.log(asteroidRefs.current); // Should log an array of asteroid DOM elements
 
 
 
@@ -104,13 +191,14 @@ function ScrollAnimations()
 
     return (
 
-        <div className='container'>
+        <div className='container overflow-x-hidden overflow-y-hidden'>
 
-        <section class="panel bg-venus-bg-reg w-screen h-screen flex justify-center items-center relative">
+        <section className="panel bg-venus-bg-reg w-screen h-screen flex justify-center items-center relative">
+            <div className="overlay absolute opacity-0 inset-0 bg-transparent"></div>
             
             
-            <div class='line-container w-full h-full flex flex-col gap-2 justify-center items-center'>
-                <span class="line w-1/2 h-full p-2 m-auto relative  inline-block bg-main-black"></span>
+            <div className='line-container w-full h-full flex flex-col gap-2 justify-center items-center'>
+                <span className="line w-1/2 h-full p-2 m-auto relative  inline-block bg-main-black"></span>
 
                 {/* Asteroid */}
                 <img
@@ -135,17 +223,98 @@ function ScrollAnimations()
 
         {/* text animation */}
         <section className='planet-animation1 bg-default-bg w-screen h-screen flex justify-center items-center relative'>
-            <h1 className='intensify font-header font-bold text-white text-xl'>animation 2</h1>
+            <div className='intensify mars-container flex w-full h-fit pt-12 flex-col md:flex-row pl-5 pr-5'>
+                    <div id='mars-pic' className='mt-14'>
+                        <img className="w/[100px] sm:w/[100px] md:w/[100px] lg:w/[150px]" src={MarsGif} alt="Venus Gif"/>
+                    </div>
+                    <div id ='mars-text' className='flex w-fit md:w-72 h-fit bg-white rounded-md font-body text-wrap p-5 text-xs md:text-sm'>
+                        War goddamit, war!
+                    </div>
+                </div>
+                {/* <h1 className='intensify font-header font-bold text-white text-xl'>animation 2</h1> */}
         </section>
 
 
 
 
-        <section className='planet-animation2 bg-saturn-bg-reg w-screen h-screen flex justify-center items-center relative'>
-            <h1 className=' font-header font-bold text-white text-xl'>animation 3</h1>
+        <section className="planet-animation2 bg-saturn-bg-reg w-screen h-screen flex justify-center items-center relative">
+            <h1 className="font-header font-bold text-white text-xl absolute top-10">Animation 3</h1>
+
+            {/* Mars and Projectiles */}
+            <div ref={marsRef} className="shooting-container flex relative justify-center items-center relative w-full h-full">
+            <div id="mars-pic" className="absolute">
+                <img className="w-[100px] sm:w-[100px] md:w-[100px] lg:w-[150px]" src={MarsGif} alt="Mars Gif" />
+            </div>
+            <div
+                id="mars-text"
+                className="absolute bottom-10 flex justify-center w-fit md:w-72 h-fit bg-white rounded-md font-body text-wrap p-5 text-xs md:text-sm"
+            >
+                War goddamit, war!
+            </div>
+
+            {/* Shooting Projectiles */}
+            <div className="projectile-container">
+                <div
+                ref={projectileRef}
+                className="projectile bg-mars-red w-8 h-8 rounded-full absolute left-[calc(50%-40px)] top-[calc(50%-40px)]"
+                />
+            </div>
+            </div>
+
+            {/* Asteroids */}
+            <div className="asteroids-container">
+            <img
+                ref={(el) => (asteroidRefs.current[0] = el)}
+                className="asteroid w-12 h-12 absolute top-[-50px] left-[50vw]"
+                src={AsteroidImg1}
+                alt="Asteroid"
+            />
+            <img
+                ref={(el) => (asteroidRefs.current[1] = el)}
+                className="asteroid w-12 h-12 absolute top-[-50px] left-[55vw]"
+                src={AsteroidImg1}
+                alt="Asteroid"
+            />
+            <img
+                ref={(el) => (asteroidRefs.current[2] = el)}
+                className="asteroid w-12 h-12 absolute top-[-50px] left-[60vw]"
+                src={AsteroidImg1}
+                alt="Asteroid"
+            />
+            <img
+                ref={(el) => (asteroidRefs.current[3] = el)}
+                className="asteroid w-12 h-12 absolute top-[-50px] left-[60vw]"
+                src={AsteroidImg1}
+                alt="Asteroid"
+            />
+            <img
+                ref={(el) => (asteroidRefs.current[4] = el)}
+                className="asteroid w-12 h-12 absolute top-[-50px] left-[60vw]"
+                src={AsteroidImg1}
+                alt="Asteroid"
+            />
+            <img
+                ref={(el) => (asteroidRefs.current[5] = el)}
+                className="asteroid w-12 h-12 absolute top-[-50px] left-[60vw]"
+                src={AsteroidImg1}
+                alt="Asteroid"
+            />
+            </div>
+      </section>
+
+
+      {/* text animation */}
+      <section className='planet-animation3 bg-default-bg w-screen h-screen flex justify-center items-center relative'>
+            <div className='intensify venus-container flex w-full h-fit pt-12 flex-col md:flex-row pl-5 pr-5'>
+                    <div id='venus-pic' className='mt-14'>
+                        <img className="w/[100px] sm:w/[100px] md:w/[100px] lg:w/[150px]" src={VenusGifMouthOpen} alt="Venus Gif"/>
+                    </div>
+                    <div id ='venus-text' className='flex w-fit md:w-72 h-fit bg-white rounded-md font-body text-wrap p-5 text-xs md:text-sm'>
+                        War goddamit, war!
+                    </div>
+                </div>
+                {/* <h1 className='intensify font-header font-bold text-white text-xl'>animation 2</h1> */}
         </section>
-
-
 
 
 
